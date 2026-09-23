626f8cc78c1c03e91313ea2765312000190f668a
way4out <way_out4@yahoo.com>
2026-09-23 13:57:50 -0700

    Pass 3 cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="rlar-v3";
+const CACHE="rlar-v4";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
